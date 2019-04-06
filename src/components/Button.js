e11d3feb243b7798902fd42c126c257b8bee8810
js-tut <johnsmilga@gmail.com>
2019-04-05 23:34:11 -0700

    9-variables

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
+const color = "#F15025";
+const transition = time => `all ${time} linear`;
 export const ButtonWrapper = styled.button`
-  color: red;
   font-size: 2rem;
-  background: red;
-  color: white;
+  background: ${color};
+  color: var(--primaryColor);
+  transition: ${transition("2s")};
+  &:hover {
+    background: black;
+  }
 `;
