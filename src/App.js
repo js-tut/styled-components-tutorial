1bdea90e0c7d70bf4793d98a3f043ad4c39d2cb3
js-tut <johnsmilga@gmail.com>
2019-04-05 22:16:34 -0700

    5-basic css styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import Heading from "./components/Heading";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1 style={{ color: "red" }}>i'm inline style</h1>
+        <h2 className="second">i'm app css style</h2>
+        <h3 className="property">i'm from css variables</h3>
+        <Heading />
       </div>
     );
   }
---
+++ src/components/Heading.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Heading.css";
+export default function Heading() {
+  return <h1 className="third">i'm separate css file</h1>;
+}
