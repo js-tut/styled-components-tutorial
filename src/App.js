d9ff1ae85243af1230f7f9beb73c9c4336671ad5
js-tut <johnsmilga@gmail.com>
2019-04-06 18:33:43 -0700

    13-global-styles

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import mainBcg from "./images/mainBcg.jpeg";
 import secondaryBcg from "./images/secondaryBcg.jpeg";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+  body {
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+
+  }
+`;
 class App extends Component {
   render() {
     return (
       <>
+        <GlobalStyle />
         <Header img={mainBcg}>
           <Banner title="fresh coffee is the best">
             <h3>hello world</h3>
